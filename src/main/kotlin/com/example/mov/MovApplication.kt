e81c408cab792e400cfaf8768fdca31687321f63
dongwoo <woomacho@gmail.com>
2023-08-26 11:18:55 +0900

    first

---
+++ src/main/kotlin/com/example/mov/MovApplication.kt
@@ -0,0 +1,11 @@
+package com.example.mov
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MovApplication
+
+fun main(args: Array<String>) {
+    runApplication<MovApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/mov/MovApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.mov
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MovApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
